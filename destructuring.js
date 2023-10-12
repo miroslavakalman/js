301889291d3a4da269603ce422fb225dd074519e
miroslavakalman <145531749+miroslavakalman@users.noreply.github.com>
2023-10-12 13:26:54 +0300

    Update destructuring.js

--- destructuring.js
+++ destructuring.js
@@ -170,7 +170,13 @@ function func({color, width, height}) {
 }
 
 func( {color: 'red', width: 400, height: 500} );
-*/
-*/
 
+//2
+function func({color = 'black', width, height}) {
+    console.log(color);
+    console.log(width);
+    console.log(height);
+}
+func( { color: 'red', width: 400, height: 500} );
+*/
 
